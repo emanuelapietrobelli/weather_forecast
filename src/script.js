d9c1bb119106bfd5f7f81824d5380b4c64c434e4
Emanuela Pietrobelli <126484374+emanuelapietrobelli@users.noreply.github.com>
2023-04-11 22:27:19 +0200

    change axios link because of athena

--- src/script.js
+++ src/script.js
@@ -96,12 +96,12 @@ search ("Berlin")
 // currently LOCATION BUTTON
 
 function showPosition(position) {
-  let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let apikey = `445905dadb3d2b0c6f1b916c9d0e3860`;
+  let latitude = position.coords.latitude;
+  let apikey = `54dfa93065b856fb699c1cd97e0803c3`;
   let units = `metric`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=${units}`;
-
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apikey}&units=${units}`  ;
+  
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
@@ -110,7 +110,7 @@ function getCurrentPosition(){
 }
 
 let button = document.querySelector("#current-location-button")
-button.addEventListener("click", getCurrentPosition)
+button.addEventListener("click", getCurrentPosition());
 
 // currently LOCATION BUTTON
 
